8fab361f86d1367a80dd5719deef0800bd120857
Konrad <kocasp@gmail.com>
2015-11-07 15:44:26 +0100

    Change table look

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'capybara'
 gem 'poltergeist'
 gem 'concurrent-ruby'
-gem 'will_paginate'
+gem 'will_paginate-bootstrap'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,22 @@
 class Course < ActiveRecord::Base
 	belongs_to :connection
+
+	def departure_dayname
+		case departure_time.wday
+		when 1
+			"poniedziałek"
+		when 2
+			"wtorek"
+		when 3
+			"środa"
+		when 4
+			"czwartek"
+		when 5
+			"piątek"
+		when 6
+			"sobota"
+		when 0
+			"niedziela"
+		end
+	end
 end
