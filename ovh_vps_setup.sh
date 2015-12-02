0ef168e2b62d1f5b7650256f8f928b7339f7d0b3
Konrad <kocasp@gmail.com>
2015-12-02 23:02:04 +0100

    Add. Sh ovh vps create script

---
+++ ovh_vps_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# USERNAME=someUser
+# HOSTS="host1 host2 host3"
+# SCRIPT="pwd; ls"
+# for HOSTNAME in ${HOSTS} ; do
+#     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+# done
+#
+#
+# BEFORE RUNNING SCRIPT MAKE SURE:
+# - server address has not been added to known hosts, it can cause WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!
+# if yes, remove it.
+# -
+
+# TODO : write script
+#
+#
+# echo "#################################################"
+# echo "SETTING UP RAILS APPLICATION ON OVH.PL VPS SERVER"
+# echo "#################################################"
+
+# CREATEDEPLOYUSER= ""
+# ssh -l "root" "vps224384.ovh.net" "ls" >> ~/mysh/top_out
+
+#ssh-copy-id deploy@vps224384.ovh.net
+
+#INSTALL_RUBY ""
