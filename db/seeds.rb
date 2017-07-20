dfa7d9729fdd489b7412079e6fbe75cc98d67181
Konrad <kocasp@gmail.com>
2017-07-20 18:10:28 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+krakow = Station.create(name: "Kraków Główny")
+warszawa = Station.create(name: "Warszawa Centralna")
+krakow_warszawa = Connection.create(station: krakow, connected_station: warszawa)
+
+kurs_1 = Course.create(departure_time: Time.zone.now, arrival_time: Time.zone.now+1.day, connection: krakow_warszawa, price: 199.99)
+kurs_2 = Course.create(departure_time: Time.zone.now+1.day, arrival_time: Time.zone.now+2.day, connection: krakow_warszawa, price: 12.99)
